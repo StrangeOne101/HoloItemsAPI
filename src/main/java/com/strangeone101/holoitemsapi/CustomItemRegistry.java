daa030956228592320bee1b864f2d45038ad7209
lythox <81324904+lythoxx@users.noreply.github.com>
2022-10-10 16:49:37 +0200

    Update CustomItemRegistry.java

--- src/main/java/com/strangeone101/holoitemsapi/CustomItemRegistry.java
+++ src/main/java/com/strangeone101/holoitemsapi/CustomItemRegistry.java
@@ -9,7 +9,6 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
